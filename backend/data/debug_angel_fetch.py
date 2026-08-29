306033067c92fb36c15fdb9c5533d3630a7eef38
millan <meet2millan.com>
2026-08-30 02:14:59 +0530

    chore: test angel fetch for tatamotors

---
+++ backend/data/debug_angel_fetch.py
@@ -0,0 +1,25 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+from backend.data.fetcher import get_token_info, ensure_session, smartApi, _call_api, _load_scrip_master, _scrip_map, clear_ticker_history, fetch_stock_data
+
+_load_scrip_master(force=True)
+info = get_token_info("TATAMOTORS")
+print("TOKEN INFO FOR TATAMOTORS:", info)
+
+ensure_session()
+if smartApi and info:
+    param = {
+        "exchange": info.get("exch_seg", "NSE"),
+        "symboltoken": str(info.get("token")),
+        "interval": "ONE_DAY",
+        "fromdate": "2025-01-01 09:15",
+        "todate": "2026-08-29 15:30"
+    }
+    print("Testing Angel API call with param:", param)
+    res = _call_api(smartApi.getCandleData, param)
+    print("Angel API response status:", res.get("status") if res else None)
+    if res and res.get("data"):
+        print("Angel candles count:", len(res["data"]))
+        print("Last candle:", res["data"][-1])
+    else:
+        print("Angel response error:", res)
