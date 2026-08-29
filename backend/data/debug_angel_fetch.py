605c829d9b6390155048974ecdb868db2f329a33
millan <meet2millan.com>
2026-08-30 02:16:15 +0530

    find tata symbols in scripmaster

--- backend/data/debug_angel_fetch.py
+++ backend/data/debug_angel_fetch.py
@@ -1,28 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-from backend.data.fetcher import get_token_info, ensure_session, smartApi, _call_api, _load_scrip_master, fetch_stock_data
-from backend.data.database import clear_ticker_history
+from backend.data.fetcher import _load_scrip_master, _scrip_map
 
 _load_scrip_master(force=True)
-info = get_token_info("TATAMOTORS")
-print("TOKEN INFO FOR TATAMOTORS:", info)
-
-ensure_session()
-if smartApi and info:
-    param = {
-        "exchange": info.get("exch_seg", "NSE"),
-        "symboltoken": str(info.get("token")),
-        "interval": "ONE_DAY",
-        "fromdate": "2025-01-01 09:15",
-        "todate": "2026-08-29 15:30"
-    }
-    print("Testing Angel API call with param:", param)
-    res = _call_api(smartApi.getCandleData, param)
-    print("Angel API response status:", res.get("status") if res else None)
-    if res and res.get("data"):
-        print("Angel candles count:", len(res["data"]))
-        print("Last 3 candles from Angel One:")
-        for c in res["data"][-3:]:
-            print(c)
-    else:
-        print("Angel response error:", res)
+tata_matches = {k: v for k, v in _scrip_map.items() if "TATA" in k.upper()}
+print("Total TATA keys in Angel One ScripMaster:", len(tata_matches))
+for k in sorted(tata_matches.keys())[:25]:
+    item = tata_matches[k]
+    print(f"Key: {k:20} -> Symbol: {item.get('symbol')} | Token: {item.get('token')} | Name: {item.get('name')}")
